90d62b2168bd64684b56403ca1a5d4ad3d94fb8b
BHARDWAJRITESH <ritesh844581@gmail.com>
2021-09-05 02:24:09 +0530

    Ability to edit Contact to AddressBook

--- src/com/bridgelabs/AddressBookManagementSystem/Textbook.java
+++ src/com/bridgelabs/AddressBookManagementSystem/Textbook.java
@@ -11,7 +11,7 @@ class Textbook {
 			int cs;
 			
 			while (true) {
-				input = JOptionPane.showInputDialog(" Enter 1 to add " + "\n Enter 2 to search " + "\n Enter 3 to Exit");
+				input = JOptionPane.showInputDialog(" Enter 1 to add " + "\n Enter 2 to search  \n Enter 3 to edit " + "\n Enter 4 to Exit");
 				cs = Integer.parseInt(input);
 				
 				switch (cs) {
@@ -22,7 +22,11 @@ class Textbook {
 					s = JOptionPane.showInputDialog("Enter name to search");
 					ab.searchPerson(s);
 					break;
-				case 3: System.exit(0);
+				case 3:
+					s=JOptionPane.showInputDialog("Enter name to edit");
+					ab.editPerson(s);
+					break;	
+				case 4: System.exit(0);
 				}
 			}
 	}
